42015617f55623c154c89f56610bd4a067bca308
OQdevpy <o.qosimov@tuit.uz>
2023-01-14 22:08:18 +0500

    statictemp

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-ithz@*q#q7z*h1x#0)ia5hc9km1*t1p^lrvffnzr3k8z-knvpl
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -124,6 +124,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = "/static/"
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- backend/src/apps/accounts/urls.py
+++ backend/src/apps/accounts/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from .views import index_page
 
 urlpatterns = [
-    path("index/", index_page, name="index_page"),
+    path("", index_page, name="index_page"),
 ]
