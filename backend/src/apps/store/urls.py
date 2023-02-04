e8ff6966a1779dd0a7a711ccb072aa6548cf8f7e
Ozodbek <johnfreedom99600@gmail.com>
2023-02-04 16:57:10 +0500

    some changes in views

--- backend/src/apps/store/urls.py
+++ backend/src/apps/store/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 
-from .views import product_list_view
+from .views import product_list,product_list_view, search, product_detail_view
 
 urlpatterns = [
+    path("list/", product_list, name="product_list_view"),
     path("list/", product_list_view, name="product_list_view"),
+    path("list/<slug:category_slug>/", product_list_view, name="product_list_view"),
+    path("search/", search, name="search"),
+    path("<slug:category_slug>/<slug:product_slug>/", product_detail_view, name="product_detail_view"),
 ]
