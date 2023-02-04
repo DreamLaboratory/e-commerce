3400b267c1f9310b64e2dc3822a23b7e1cac1fdb
Davronbek Boltayev <xackercoder@gmail.com>
2023-02-04 22:45:37 +0500

    * signals * custom command * Avg, aggregate * related_name

--- backend/src/apps/store/models/product.py
+++ backend/src/apps/store/models/product.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
+# Avg, Count, Min, Sum
+from django.db.models import Avg
+
 # Create your models here.
 from ...common.models import BaseModel
 from ...common.file_renamer import PathAndRename
@@ -18,10 +21,10 @@ class Product(BaseModel):
     slug = models.SlugField(_("Slug"), max_length=255, unique=True, db_index=True, blank=True)
     description = models.TextField(max_length=1000, blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    image = models.ImageField(upload_to=path_and_rename)
+    image = models.ImageField(upload_to=path_and_rename, blank=True, null=True)
     stock = models.PositiveIntegerField()
     is_available = models.BooleanField(default=True, help_text="Is product available?")
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="products", blank=True, null=True)
 
     class Meta:
         verbose_name = "product"
@@ -35,14 +38,28 @@ class Product(BaseModel):
     def __str__(self):
         return self.name
 
-    def save(self):
-        self.slug = slugify(self.name)
+    @property
+    def get_image_url(self):
+        return self.image.url if self.image and hasattr(self.image, "url") else "#"
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
         super(Product, self).save()
+
+    @property
+    def average_rating(self):
+        reviews = self.reviews.filter(status=True).aggregate(Avg("rating"))
+        return float(reviews["rating__avg"]) if reviews["rating__avg"] else 0
 
 
 class ProductImage(BaseModel):
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="images")
     image = models.ImageField(upload_to=path_and_rename)
+
+    @property
+    def get_image_url(self):
+        return self.image.url if self.image and hasattr(self.image, "url") else "#"
 
     class Meta:
         verbose_name = "Product image"
--- backend/src/apps/store/views/product.py
+++ backend/src/apps/store/views/product.py
@@ -5,8 +5,7 @@ from django.db.models import Q
 from django.shortcuts import render
 
 from ..models.category import Category
-from ..models.product import Product, ProductImage
-from ..models.review import Review
+from ..models.product import Product
 
 # import logging
 
@@ -15,14 +14,12 @@ logger = logging.getLogger(__name__)
 
 
 def product_list_view(request, category_slug=None):
-    # TODO try except
     try:
         min_price = request.GET.get("min")
         max_price = request.GET.get("max")
 
         products = Product.objects.filter(is_available=True)
         if min_price and max_price:
-            # __gte = greater than or equal to >= and __lte = less than or equal to <=
             products = products.filter(price__gte=min_price, price__lte=max_price)
         if category_slug:  # TODO - add category filter
             category = Category.objects.get(slug=category_slug)
@@ -41,9 +38,10 @@ def product_list_view(request, category_slug=None):
 
 # url/<slug:category_slug>/<slug:product_slug>/
 def product_detail_view(request, category_slug, product_slug):
+    # TODO try except
     product = Product.objects.get(category__slug=category_slug, slug=product_slug)
-    product_reviews = Review.objects.filter(product=product)
-    product_images = ProductImage.objects.filter(product=product)
+    product_reviews = product.reviews.filter(status=True)
+    product_images = product.images.all()
     context = {
         "product": product,
         "product_reviews": product_reviews,
@@ -63,5 +61,4 @@ def search(request):
         "products": products,
         "product_count": product_count,
     }
-    print(context)
     return render(request, "store/store.html", context)
