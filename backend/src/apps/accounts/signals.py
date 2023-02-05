2f3000e253b6de23dfe0bf04b947b88407049e33
Ozodbek <johnfreedom99600@gmail.com>
2023-02-05 19:21:56 +0500

    added senf email to signal

--- backend/src/apps/accounts/signals.py
+++ backend/src/apps/accounts/signals.py
@@ -1,9 +1,13 @@
-# post_save bazaga saqlab bulgandan sung ishga tushadi
-# pre_save - bazaga saqlashidan oldin ishga tushadi
-
 from django.db.models.signals import post_save
 from .models import Account, UserProfile
 from django.dispatch import receiver
+from django.contrib.sites.shortcuts import get_current_site
+from django.template.loader import render_to_string
+from django.utils.encoding import force_bytes
+from django.utils.html import strip_tags
+from django.utils.http import urlsafe_base64_encode
+import asyncio
+from ..common.send_email import send_email_async
 
 
 @receiver(post_save, sender=Account)
@@ -11,3 +15,23 @@ def create_user_profile(sender, instance, created, **kwargs):
     if created:
         UserProfile.objects.create(user=instance)
         # TODO send email to user
+
+        email = instance.email
+        username = email.split("@")[0]  # get username from email
+        instance.username = username
+        instance.save()
+        domain = '127.0.0.1:8000'
+        domain = f"http://{domain}/activate/{urlsafe_base64_encode(force_bytes(instance))}/"
+        subject = "Welcome to the site"
+        message = f"Hi {instance.firstname}, welcome to the site"
+        body = render_to_string(
+            "accounts/verification.html",
+            {
+                "subject": subject,
+                "body": message,
+                "domain": domain,
+            },
+        )
+        html_body = strip_tags(body)
+        asyncio.run(send_email_async(subject, html_body, [email]))
+
