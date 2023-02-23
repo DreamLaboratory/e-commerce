f7bbbf4f3069f74bcd1d48e06466172277149c98
Davronbek Boltayev <xackercoder@gmail.com>
2023-02-23 22:04:49 +0500

    add new futures

--- backend/src/apps/order/views/checkout.py
+++ backend/src/apps/order/views/checkout.py
@@ -14,9 +14,9 @@ def checkout(request):
         cart = Cart.objects.filter(cart_id_pk=_cart_id(request)).first()
     cart_items = CartItem.objects.filter(cart=cart, status=StatusChoices.ACTIVE)
     # annotate
-    cart_item = cart_items.annotate(total_price=F("product__price") * F("quantity"))
-    total_price = cart_item.aggregate(Sum("total_price"))
-    total_price = total_price["total_price__sum"] or 0
+    cart_item = cart_items.annotate(price=F("product__price") * F("quantity"))
+    total_price = cart_item.aggregate(Sum("price"))
+    total_price = total_price["price__sum"] or 0
 
     delevery = Decimal(total_price * Decimal(0.1)).quantize(Decimal("0.01"))  # 10% of total price
 
--- backend/src/apps/order/views/order_list.py
+++ backend/src/apps/order/views/order_list.py
@@ -1,1 +1,9 @@
-# my order
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from ..models.order import Order
+
+
+@login_required
+def my_order_list(request):
+    orders = Order.objects.filter(user=request.user)
+    return render(request, "order/my_order_list.html", {"orders": orders})
